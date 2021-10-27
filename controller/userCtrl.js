3ab88bee5a9b15b78b3130ef57f81018784e0480
Aram Kim <nayulpapa0201@gmail.com>
2021-10-27 10:43:09 -0600

    password encrypt

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,10 +1,17 @@
 const Users = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 const useCtrl = {
-  registerUser: (req, res) => {
+  registerUser: async (req, res) => {
     try {
-      res.json(req.body);
-      res.json({ msg: "Sign up Success" });
+      const { username, email, password } = req.body;
+      const user = await Users.findOne({ email: email });
+      if (user)
+        return res.status(400).json({ msg: "The email already exists." });
+
+      const passwordHash = await bcrypt.hash(password, 10);
+      res.json({ passwordHash });
+      //   res.json({ msg: "Sign up Success" });
     } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
